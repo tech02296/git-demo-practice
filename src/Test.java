89511e5d7b77d0f7018eebe240a8757d4bc46669
Happy-Tester <146895182+Nik-prajap@users.noreply.github.com>
2024-01-27 15:16:27 +0000

    commit8

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("My git demo hio");
+        System.out.println("            **");
+            System.out.println("    **              **");
+            System.out.println("**      **      **      **");
+            System.out.println("    **      **      **");
+            System.out.println("**      **      **      **");
+            System.out.println("    **              **");
+            System.out.println("            **");
     }
 }
